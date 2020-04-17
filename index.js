242e49fe6ceae17da1b597c5a1f38b0a5aaa96b7
Adam Johnson <adam@chromative.net>
2020-04-17 14:05:16 -0500

    express and page example
    Express is added and an example of what the application should look like has been added to html with the apple as an example

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const path = require('path');
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.use(express.static(path.join(__dirname,'public')));
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, ()=> console.log(`Example app listening at http://localhost:${port}`));
