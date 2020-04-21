4ce625f6e2d4e9e2720e6bd3b8229570805ef8cc
Adam Johnson <adam@chromative.net>
2020-04-21 16:28:57 -0500

    add food randomization and images
    Instead of having the foods in linear order they are now shuffled.  In addition, the unsplash attribution element has been added to give credit to images.

--- Foods.js
+++ Foods.js
@@ -4,49 +4,57 @@ const foods = [
         id: 1,
         name: 'Apple',
         category: 'fruit',
-        image: 'apple.jpg'
+        image: 'apple.jpg',
+        imageAttribution: ['@louishansel','Louis Hansel @shotsoflouis']
     },
     {
         id: 2,
         name: 'Orange',
         category: 'fruit',
-        image: 'orange.jpg'
+        image: 'orange.jpg',
+        imageAttribution: ['@runblue','Xiaolong Wong']
     },
     {
         id: 3,
         name: 'Banana',
         category: 'fruit',
-        image: 'banana.jpg'
+        image: 'banana.jpg',
+        imageAttribution: ['@giorgiotrovato','Giorgio Trovato']
     },
     {
         id: 4,
         name: 'Pomegranite',
         category: 'fruit',
-        image: 'pomegranite.jpg'
+        image: 'pomegranite.jpg',
+        imageAttribution: ['@matyszczyk','Marta Matyszczyk']
     },
     {
         id: 5,
         name: 'Coffee',
         category: 'beverage',
-        image: 'coffee.jpg'
+        image: 'coffee.jpg',
+        imageAttribution: ['@asthetik','Mike Kenneally']
     },
     {
         id: 6,
         name: 'Hamburger',
         category: 'sandwich',
-        image: 'hamburger.jpg'
+        image: 'hamburger.jpg',
+        imageAttribution: ['@amir_v_ali','amirali mirhashemian']
     },
     {
         id: 7,
         name: 'Macaroni and cheese',
         category: 'pasta',
-        image: 'macandcheese.jpg'
+        image: 'macandcheese.jpg',
+        imageAttribution: ['@hermez777','Hermes Rivera']
     },
     {
         id: 8,
         name: 'Sunny side up eggs',
         category: 'eggs',
-        image: 'sunnysideupeggs.jpg'
+        image: 'sunnysideupeggs.jpg',
+        imageAttribution: ['@saraheboudreau','Sarah Boudreau']
     },
 ];
 
--- public/app.js
+++ public/app.js
@@ -1,20 +1,35 @@
 let id = 0;
 let resultArray = [];
+let foodOrderArray = [];
 let resultString = '<h1>Results</h1>';
+let unsplashTag;
 let foodCount = 0;
 let foodImage;
 let foodName;
 
+/**
+ * The main app constructor
+ */
 function app(){
     foodImage = document.querySelector('#food-image');
     foodName = document.querySelector('#food-name');
-    //want to call show next food to replace what is currently in the HTML due to the desire to randomize the starting value
-    //food count then show next food
+    unsplashTag = document.querySelector('#unsplash-tag');
 
     setFoodCount();
+
+    //create an array with the food count and then shuffle it to randomize
+    for(let i = 1; i <= foodCount; i++)
+    {
+        foodOrderArray = [i,...foodOrderArray]
+    }
+    shuffle(foodOrderArray);
+
     showNextFood();
 }
 
+/**
+ * Get the count of foods from the server
+ */
 function setFoodCount(){
     let xhr = new XMLHttpRequest();
     xhr.open("GET", `/api/foods/count`, false);
@@ -29,7 +44,7 @@ function setFoodCount(){
     }
 
     xhr.onerror = function(){
-        console.log('Request Error...');
+        console.log('Request Error from setFoodCount()...');
     }
 
     xhr.send();
@@ -45,34 +60,66 @@ function submitFood(text){
     showNextFood()
 }
 
-
+/**
+ * Get the next food item from the list until there are no more, then display the results
+ */
 function showNextFood(){
-    console.log(foodCount);
     if (id < foodCount)
     {
-        id = id + 1;
+        
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", `/api/foods/${id}`, true);
+        xhr.open("GET", `/api/foods/${foodOrderArray[id]}`, true);
             
         xhr.onload = function(){
             if(this.status == 200){
                 let currentFood = JSON.parse(this.responseText);
                 foodImage.src = 'images/' + currentFood[0].image;
+                foodImage.alt = currentFood[0].name;
                 foodName.innerText = currentFood[0].name;
+                unsplashTagConstructor(currentFood[0].imageAttribution);
             } else if (this.status == 404) {
                 document.getElementById('text').innerHTML = 'Not Found';
             }
         }
 
         xhr.onerror = function(){
-            console.log('Request Error...');
+            console.log('Request Error from showNextFood()...');
         }
 
         xhr.send();
+        id = id + 1;
     }
     else{
         let content = document.querySelector('#content');
         resultArray.forEach(element => resultString = resultString + `<p>${element.name} : ${element.text}</p>`);
         content.innerHTML = resultString;
     }
+}
+
+function unsplashTagConstructor(imageAttribution) {
+
+    unsplashTag.style = "background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px";
+    unsplashTag.href = `https://unsplash.com/${imageAttribution[0]}?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge`;
+    unsplashTag.target = "_blank";
+    unsplashTag.rel = "noopener noreferrer";
+    unsplashTag.title = `Download free do whatever you want high-resolution photos from ${imageAttribution[0]}`;
+    unsplashTag.innerHTML = `<span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">${imageAttribution[1]}</span>`;
+}
+
+/**
+ * Fisher-Yates / Knuth shuffle
+ */
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
