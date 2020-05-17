2ea44e4fd50de08ffba27f0dfd1ee55df0c212b9
Adam Johnson <adam@chromative.net>
2020-05-17 12:03:52 -0500

    fix linting issues

--- node_ts/routes/api/foods.ts
+++ node_ts/routes/api/foods.ts
@@ -1,28 +1,24 @@
 import Router from 'express-promise-router';
 
 const router: any = Router();
-const foods: any = require('../../Foods');
 const db: any = require('../../db');
 
 // Gets All Foods
-router.get('/', async(req: any, res: any) => {
+router.get('/', async (req: any, res: any) => {
   const { rows } = await db.query('SELECT * FROM foods');
-  res.send(rows)
-})
+  res.send(rows);
+});
 
-
-router.get('/count', async(req: any, res: any) => {
+router.get('/count', async (req: any, res: any) => {
   const { rows } = await db.query('SELECT count(*) FROM foods');
   res.send(rows[0].count);
-})
-
+});
 
 // Get Single Food
-
-router.get('/:id', async(req:any, res:any) => {
+router.get('/:id', async (req:any, res:any) => {
   const { id } = req.params;
   const { rows } = await db.query(`SELECT * FROM foods where id = ${id}`);
   res.send(rows[0]);
-})
+});
 
 module.exports = router;
