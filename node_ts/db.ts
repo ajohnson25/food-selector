11146286157acf718398e66e36ea19046d5000a0
Adam Johnson <adam@chromative.net>
2020-05-17 12:41:10 -0500

    postgres ssl configuration for heroku
    The deploy is now showing a page in heroku but heroku's logs are having an error with a self signed certificate.  I am changing ssl option to the one heroku recommends to see if that will fix the issue.

--- node_ts/db.ts
+++ node_ts/db.ts
@@ -2,7 +2,7 @@ export const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5433/foodSelector',
-  ssl: !!process.env.DATABASE_URL
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = {
